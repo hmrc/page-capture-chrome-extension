b7c28439a85b806184ed1787f2e65ae8698f9f36
Tom McArdle <79710668+tmc-ee@users.noreply.github.com>
2022-04-22 12:35:39 +0100

    PLATUI-1686: Update encodeExtension script

--- encodeExtension.sh
+++ encodeExtension.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-OS="`uname`"
-case $OS in
-  'Linux')
-    $(which google-chrome-stable) --pack-extension=page-capture
-    ;;
-  'Darwin')
-    /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --pack-extension=page-capture
-    ;;
-  *) ;;
-esac
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --pack-extension=page-capture
 
 base64 -i page-capture.crx
 
