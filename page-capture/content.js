06b4b63564ebc6c3ef91c71b11cf1deb518afd37
Viv Richards <1014468+vivrichards600@users.noreply.github.com>
2022-12-12 16:56:38 +0000

    PLATUI-1995 - Migrating extension from Manifest V2 to Manifest V3

--- page-capture/content.js
+++ page-capture/content.js
@@ -1,4 +1,4 @@
-var matchSrcAttLocalResource = RegExp('(^http:\/\/localhost:[0-9]{4,5})|(^[\.\/]{1}[a-zA-Z]{1}[\/a-zA-Z0-9-\.]+$)')
+let matchSrcAttLocalResource = RegExp('(^http:\/\/localhost:[0-9]{4,5})|(^[\.\/]{1}[a-zA-Z]{1}[\/a-zA-Z0-9-\.]+$)')
 
 function absoluteUrl(url) {
   const link = document.createElement("a");
